4f5ea224f7361fb263c442f25a4e1133830d9d25
EgydioBNeto <egydio.neto@tmaior.com.br>
2024-03-14 18:13:58 -0300

    Workspaces

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "rocketseat-bucket-iac"
+  bucket = "rocketseat-bucket-iac-${terraform.workspace}"
 
   tags = {
     Name    = "Primeiro bucket"
     Iac     = "true"
+    context = "${terraform.workspace}"
   }
 }
