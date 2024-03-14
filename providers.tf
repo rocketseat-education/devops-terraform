64b9338c4716d48382f2c75bdcf5ca71e1a129af
EgydioBNeto <egydio.neto@tmaior.com.br>
2024-03-14 19:16:41 -0300

    Mantendo o Estado na nuvem

--- providers.tf
+++ providers.tf
@@ -5,6 +5,20 @@ terraform {
       version = "5.36.0"
     }
   }
+  backend "s3" {
+    bucket  = "rocketseat-state-bucket-tf"
+    region  = "us-east-2"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
 }
 
 provider "aws" {}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "state_bucket" {
+  type        = string
+  default     = "rocketseat-state-bucket-tf"
+  description = "Bucket com o estado"
+}
