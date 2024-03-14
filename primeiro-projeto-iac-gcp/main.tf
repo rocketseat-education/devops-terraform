d0722de8b45f9c01451735a58d84b6021eefcea5
EgydioBNeto <egydio.neto@tmaior.com.br>
2024-03-14 19:33:24 -0300

    Conhecendo e configurando provider - GCP

---
+++ primeiro-projeto-iac-gcp/main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "vm" {
+  name         = "rocketseat-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
---
+++ primeiro-projeto-iac-gcp/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "crafty-stock-415315"
+  region  = "us-central1"
+}
